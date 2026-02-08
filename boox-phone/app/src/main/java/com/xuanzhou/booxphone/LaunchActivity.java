77242c9a15751760db9ce9f11e9e28926b10c8a0
XuanZhou <qz@entromoonic.com>
2026-02-08 13:32:42 +0800

    Phone shortcut: open main screen instead of dial pad

--- boox-phone/app/src/main/java/com/xuanzhou/booxphone/LaunchActivity.java
+++ boox-phone/app/src/main/java/com/xuanzhou/booxphone/LaunchActivity.java
@@ -1,6 +1,7 @@
 package com.xuanzhou.booxphone;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -9,7 +10,11 @@ public class LaunchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(Intent.ACTION_DIAL);
+        // 打开拨号器主界面（通话记录），而不是拨号键盘
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setComponent(new ComponentName(
+            "org.codeaurora.dialer",
+            "com.android.dialer.main.impl.MainActivity"));
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         try {
